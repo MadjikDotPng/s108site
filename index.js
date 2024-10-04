86bebf4050db2e04bf8803757e2643d442bc6bbb
Holden Parker <madjiktv58@gmail.com>
2024-10-04 15:52:05 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function isMobile(){
+    // credit to Timothy Huang for this regex test: 
+    // https://dev.to/timhuang/a-simple-way-to-detect-if-browser-is-on-a-mobile-device-with-javascript-44j3
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        window.location.href = "/m/index.html"
+        return true;
+   }
+   else{
+        return false
+   }
+} 
